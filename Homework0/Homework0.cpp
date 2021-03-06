f51816eb053c2cb7b8d94a8a2257304db480745c
zion <1399264684@qq.com>
2021-03-06 19:09:02 +0800

    Homework1 and install opencv

--- Homework0/Homework0.cpp
+++ Homework0/Homework0.cpp
@@ -1,13 +1,18 @@
 #include <Eigen/Core>
-#include <Eigen/Dense>
 #include <iostream>
 #include <cmath>
+#include <opencv2/opencv.hpp>
 #define PI acos(-1)
 using namespace Eigen;
 using namespace std;
+using namespace cv;
 
 int main()
 {
+    Mat a;
+    Mat srcImage = imread("C:/Users/13992/Desktop/12.png"); 
+    imshow("Test OpenCV", srcImage);
+    waitKey(0);
     MatrixXf point(3, 1);
     MatrixXf transformation(3, 3);
     point << 2,
@@ -16,7 +21,7 @@ int main()
     transformation << cos(45.0 / 180.0 * PI), -sin(45.0 / 180.0 * PI), 1,
                       sin(45.0 / 180 * PI),   cos(45.0 / 180 * PI),    2,
                       0,                      0,                       1;
-    cout << transformation * point;
+    cout << transformation * point << endl;
     system("pause");
     return 0;
-}                  
+}
