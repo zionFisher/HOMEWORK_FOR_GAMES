898817b3079392736a2698eb87a7c0d2c680f253
zion <1399264684@qq.com>
2021-04-13 15:52:47 +0800

    Homework4 submission

--- Homework4/Code Framework/main.cpp
+++ Homework4/Code Framework/main.cpp
@@ -1,4 +1,5 @@
 #include <chrono>
+#include <cmath>
 #include <iostream>
 #include <opencv2/opencv.hpp>
 
@@ -48,10 +49,73 @@ cv::Point2f recursive_bezier(const std::vector<cv::Point2f> &control_points, flo
 
 void bezier(const std::vector<cv::Point2f> &control_points, cv::Mat &window) 
 {
+    float frame_buf[700][700] = {0};
+
+    // Antialiasing
     for (double t = 0.0; t <= 1.0; t += 0.001) 
     {
-        auto point = recursive_bezier(control_points, t);
-        window.at<cv::Vec3b>(point.y, point.x)[2] = 255;
+        cv::Point2f point = recursive_bezier(control_points, t);
+
+        float x = point.x;
+        float y = point.y;
+        int xi = static_cast<int>(x);
+        int yi = static_cast<int>(y);
+
+        if (x - xi < 0.5 && y - yi < 0.5)
+        {
+
+        }
+        else if (x - xi < 0.5 && y - yi > 0.5)
+        {
+            yi++;
+        }
+        else if (x - xi > 0.5 && y - yi < 0.5)
+        {
+            xi++;
+        }
+        else if (x - xi > 0.5 && y - yi > 0.5)
+        {
+            xi++; yi++;
+        }
+
+        cv::Point2i RTpoint(xi,     yi);
+        cv::Point2i RBpoint(xi,     yi - 1);
+        cv::Point2i LTpoint(xi - 1, yi);
+        cv::Point2i LBpoint(xi - 1, yi - 1);
+
+        float RTdistToPoint = sqrt((x - RTpoint.x) * (x - RTpoint.x) + (y - RTpoint.y) * (y - RTpoint.y));
+        float RBdistToPoint = sqrt((x - RBpoint.x) * (x - RBpoint.x) + (y - RBpoint.y) * (y - RBpoint.y));
+        float LTdistToPoint = sqrt((x - LTpoint.x) * (x - LTpoint.x) + (y - LTpoint.y) * (y - LTpoint.y));
+        float LBdistToPoint = sqrt((x - LBpoint.x) * (x - LBpoint.x) + (y - LBpoint.y) * (y - LBpoint.y));
+
+        if ((1 - RTdistToPoint / sqrt(2)) * 255 > frame_buf[RTpoint.x][RTpoint.y])
+            frame_buf[RTpoint.x][RTpoint.y] = (1 - RTdistToPoint / sqrt(2)) * 255;
+        if ((1 - RBdistToPoint / sqrt(2)) * 255 > frame_buf[RBpoint.x][RBpoint.y])
+            frame_buf[RBpoint.x][RBpoint.y] = (1 - RBdistToPoint / sqrt(2)) * 255;
+        if ((1 - LTdistToPoint / sqrt(2)) * 255 > frame_buf[LTpoint.x][LTpoint.y])
+            frame_buf[LTpoint.x][LTpoint.y] = (1 - LTdistToPoint / sqrt(2)) * 255;
+        if ((1 - LBdistToPoint / sqrt(2)) * 255 > frame_buf[LBpoint.x][LBpoint.y])
+            frame_buf[LBpoint.x][LBpoint.y] = (1 - LBdistToPoint / sqrt(2)) * 255;
+
+        float RTpercentage = 255;
+        float RBpercentage = frame_buf[RBpoint.x][RBpoint.y];
+        float LTpercentage = frame_buf[LTpoint.x][LTpoint.y];
+        float LBpercentage = frame_buf[LBpoint.x][LBpoint.y];
+
+        cv::Vec3b RTcolor(RTpercentage, RTpercentage, RTpercentage);
+        cv::Vec3b RBcolor(RBpercentage, RBpercentage, RBpercentage);
+        cv::Vec3b LTcolor(LTpercentage, LTpercentage, LTpercentage);
+        cv::Vec3b LBcolor(LBpercentage, LBpercentage, LBpercentage);
+
+        window.at<cv::Vec3b>(RTpoint) = RTcolor;
+        window.at<cv::Vec3b>(RBpoint) = RBcolor;
+        window.at<cv::Vec3b>(LTpoint) = LTcolor;
+        window.at<cv::Vec3b>(LBpoint) = LBcolor;
+
+        // not antialiasing
+        // window.at<cv::Vec3b>(y, x)[0] = 255;
+        // window.at<cv::Vec3b>(y, x)[1] = 255;
+        // window.at<cv::Vec3b>(y, x)[2] = 255;
     }
 }
 
